9fb5639aaa01599752f75526d27b78f7d8a5f031
Sean Esqueda <sesqueda99@aol.com>
2019-08-23 00:22:07 -0700

    Finish survey.html and added html & api routes

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ var path = require("path");
 
 // Set up Express
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Set up Express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log("Listening on PORT: " + PORT);
+})
