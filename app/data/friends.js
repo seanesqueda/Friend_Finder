8e1b10457d94d496efa13d6ef2c89c9ef102e032
Sean Esqueda <sesqueda99@aol.com>
2019-08-23 04:57:56 -0700

    Fixed home.html addedfriends.js and fixed apiroutes

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends = [
+	{
+		name: "Test",
+		photo: "test.jpg",
+		scores: [3, 5, 4, 2, 1, 2, 5, 1, 2, 4]
+	},
+];
+
+module.exports = friends;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,11 @@ var path = require("path");
 
 module.exports = function (app) {
 
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
-    });
-
-    app.get("/survey", function (req, res) {
+    app.get("/survey", function (err,res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
-    });
-};
+    })
+
+    app.get("/", function (err,res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    })
+}
